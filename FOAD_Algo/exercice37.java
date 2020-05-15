32397266ecc0b8ef001b4601bb95f7229146add6
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-05-15 17:47:24 +0200

    update exercice37.java
    fin développment java de l'exercice algorithme 3.7

--- FOAD_Algo/exercice37.java
+++ FOAD_Algo/exercice37.java
@@ -3,15 +3,33 @@ package crm.algorithme.exercices;
 public class exercice37 {
 
     public static void main(String[] args) {
-        String name[] = {"agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie"}, nameSearch="chloé";
-        int middleName=0, i=0, compareResult, lowIndex, hightIndex, position=-1;
+        String name[] = {"agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie"}, nameSearch="olga";
+        int middleName=0, i=0, compareResult,lowPrevIndex, lowIndex=0, hightPrevIndex, hightIndex, position=-1;
+        lowPrevIndex = 0;
+        hightPrevIndex = hightIndex = name.length;
         middleName = (int)Math.ceil(name.length/2);
-        while(position == -1 || i < name.length - 1) {
+        while(position == -1 && i < name.length - 1) {
             compareResult = nameSearch.compareToIgnoreCase(name[middleName]);
-            if(compareResult > 0) {
-                lowIndex = middleName + 1;
-                hightIndex = ????;
+            if(compareResult < 0) {
+                lowIndex = lowPrevIndex;
+                hightIndex = middleName;
+                middleName = (int)Math.ceil((hightIndex - lowIndex) / 2);
+            } else if(compareResult > 0) {
+                lowIndex = middleName;
+                hightIndex = hightPrevIndex;
+                middleName += (int)Math.ceil((hightIndex - lowIndex) / 2);
+            } else {
+                position = middleName;
             }
+            lowPrevIndex = lowIndex;
+            hightPrevIndex = hightIndex;
+
+            i++;
+        }
+        if(position == -1) {
+            System.out.println("Le nom n'a pas été trouvé");
+        } else {
+            System.out.println("Le nom " + nameSearch + " est en position n° " + position);
         }
     }
 }
