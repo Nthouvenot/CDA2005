4ce32dc55447229eabed07fd5bb5e4fc076ea92b
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-05-18 09:04:46 +0200

    update exercice 37
    suppression variable inutile + supression 1 ligne oublié dans le pseudo code

--- FOAD_Algo/exercice37.java
+++ FOAD_Algo/exercice37.java
@@ -3,26 +3,21 @@ package crm.algorithme.exercices;
 public class exercice37 {
 
     public static void main(String[] args) {
-        String name[] = {"agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie"}, nameSearch="olga";
-        int middleName=0, i=0, compareResult,lowPrevIndex, lowIndex=0, hightPrevIndex, hightIndex, position=-1;
-        lowPrevIndex = 0;
-        hightPrevIndex = hightIndex = name.length;
+        String name[] = {"agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie"}, nameSearch="chloé";
+        int middleName=0, i=0, compareResult,  lowIndex=0, hightIndex=0, position=-1;
+        hightIndex = name.length;
         middleName = (int)Math.ceil(name.length/2);
         while(position == -1 && i < name.length - 1) {
             compareResult = nameSearch.compareToIgnoreCase(name[middleName]);
             if(compareResult < 0) {
-                lowIndex = lowPrevIndex;
                 hightIndex = middleName;
                 middleName = (int)Math.ceil((hightIndex - lowIndex) / 2);
             } else if(compareResult > 0) {
                 lowIndex = middleName;
-                hightIndex = hightPrevIndex;
                 middleName += (int)Math.ceil((hightIndex - lowIndex) / 2);
             } else {
                 position = middleName;
             }
-            lowPrevIndex = lowIndex;
-            hightPrevIndex = hightIndex;
 
             i++;
         }
