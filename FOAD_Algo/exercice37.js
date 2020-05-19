c0be458c7eb23b1032c6dce1316c114a569031d7
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-05-19 17:31:47 +0200

    changement variable dans test de la niém case du tableau cause erreur de variable
    bug case 0 du tableau le nom du premier élement du tableau n'est pas trouvé par l'algo/code

--- FOAD_Algo/exercice37.js
+++ FOAD_Algo/exercice37.js
@@ -2,23 +2,24 @@ let name = ['agathe', 'berthe', 'chloé', 'cunégonde', 'olga', 'raymonde', 'sid
 let middleName = 0, i = 0, compareResult = 0, lowIndex = 0, hightIndex = 0, position = -1;
 hightIndex = name.length;
 middleName = Math.ceil(name.length / 2)
-console.log(name.length);
-while (position == -1 && console.log(i < name.length - 1)) {
-    compareResult = nameSearch.localeCompare(name[i]);
+while (position == -1 && i < name.length - 1) {
+    compareResult = nameSearch.localeCompare(name[middleName]);
+    console.log('resultat de comparaison :' + compareResult);
     if (compareResult < 0) {
         hightIndex = middleName;
         middleName = Math.ceil((hightIndex - lowIndex) / 2);
     } else if (compareResult > 0) {
         lowIndex = middleName;
-        middleName += Math.ceil((hightIndex - lowIndex) / 2)
+        middleName += Math.ceil((hightIndex - lowIndex) / 2);
     } else {
         position = middleName;
     }
+    console.log("nombre d\'itération :" + i);
     i++;
-    console.log(middleName);
 }
+console.log(position);
 if (position != -1) {
-    console.log("Le nom " + nameSearch + " est en position n° " + position)
+    console.log("Le nom " + nameSearch + " est en position n° " + position);
 } else {
-    console.log("Le nom n'a pas été trouvé")
+    console.log("Le nom n'a pas été trouvé");
 }
