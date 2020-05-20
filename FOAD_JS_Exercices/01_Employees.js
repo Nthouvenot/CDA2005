87c88fe062aa8c1c09f443f7befc407e19de40bd
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-05-20 18:31:42 +0200

    Début de réalisation de l'exercice 01_employees constructeur + premiére méthode faite

---
+++ FOAD_JS_Exercices/01_Employees.js
@@ -0,0 +1,88 @@
+/**
+ * JAVASCRIPT:  Exercices
+ * 
+ * Listing d'employés
+ * 
+ * Consignes : 
+ * A l'aide de la syntaxe orientée "classes", modélisez un objet "Employee" possédant les caractéristiques suivantes :
+ *  Attributs: 
+ *  - id (int): identifiant
+ *  - lastname (string): nom
+ *  - firstname (string): prénom
+ *  - email (string): calculé automatiquement dans le constructeur (exemple: John Doe => jdoe@email.fr)
+ *  - role (string): poste occupé
+ *  - salary (int): salaire annuel BRUT
+ *  - hiredate (Date): date d'embauche au format YYYY-MM-DD
+ * 
+ *  Méthodes:
+ *  - getMonthlySalary() : retourne un entier -> le salaire mensuel NET calculé à partir du salaire annuel (salaire_mensuel = salaire_annuel / 12 * 0.75)
+ *  - getSeniority() : renvoie une chaine -> l'ancienneté de l'employé (exemples: "19 jours", "3 mois et 2 jours", "2 ans 7 mois et 8 jours") 
+ * 
+ * Une fois modélisé et testé, complétez le tableau "employees" pour qu'il contienne 5 employés au total (ni plus, ni moins).
+ * Parcourir ensuite le tableau complété et afficher chaque employé dans la console (nom, prénom, email, ancienneté, salaire mensuel NET)
+ * Afficher ensuite, individuellement : 
+ *  - L'employé ayant le plus d'ancienneté
+ *  - L'employé avec le plus haut salaire
+ *  - L'employé avec le plus bas salaire
+ *  - La différence de salaire entre les 2 précédents 
+ * 
+ * Documentation Javascript complète: https://developer.mozilla.org/fr/docs/Web/JavaScript
+ * Tutoriel Javascript: https://www.pierre-giraud.com/javascript-apprendre-coder-cours/introduction/ 
+ */
+
+
+
+/* COMPLÉTEZ LE PROGRAMME... */
+
+/**
+ * @class Employee
+ */
+class Employee
+{
+    // A vous de jouer...
+    constructor(id, lastName, firstName, role, salary, hiredate) {
+        this.id = id;
+        this.lastName = new String(lastName);
+        this.firstName = new String(firstName); 
+        this.email = new String;
+        this.role = new String(role);
+        this.salary = salary;
+        this.hiredate = new Date(hiredate);
+    }
+
+    getMonthlySalary(salaire_annuel) {
+    salaire_mensuel = (salaire_annuel / 12) * 0.75;
+    return salaire_mensuel;
+    }
+
+    getSeniority() {
+
+    }
+    
+}
+
+
+
+/** DÉBUT ZONE NON EDITABLE (Ne pas modifier les lignes suivantes) */
+
+
+/** @var Employee employee1 */
+var employee1 = new Employee(1, 'Doe', 'John', 'manager', 82000, new Date('2020-12-28')); // création d'un nouvel employé
+
+/** @var array employees */
+const employees = [employee1]; // tableau contenant les employés
+
+console.log(employee1); // doit afficher l'objet "employee1"
+console.log("Il y a " + employees.length + " employé(e)s."); // doit afficher "Il y a 5 employé(e)s."
+console.log(employees); // export des employés dans la console
+
+
+/** FIN ZONE NON EDITABLE (Ne pas modifier les lignes précédentes) */
+
+
+
+// Écrivez votre code à partir de la ligne suivante...
+
+
+
+
