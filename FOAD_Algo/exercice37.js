befcec3b9394db6070b0e8956dd47a41dd249b00
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-05-20 16:34:49 +0200

    update exercice37.js
    remplacement des ceil par des floor + supression des messages console de debugage

--- FOAD_Algo/exercice37.js
+++ FOAD_Algo/exercice37.js
@@ -1,23 +1,20 @@
-let name = ['agathe', 'berthe', 'chloé', 'cunégonde', 'olga', 'raymonde', 'sidonie'], nameSearch = 'agathe';
+let name = ['agathe', 'berthe', 'chloé', 'cunégonde', 'olga', 'raymonde', 'sidonie'], nameSearch = 'chloé';
 let middleName = 0, i = 0, compareResult = 0, lowIndex = 0, hightIndex = 0, position = -1;
 hightIndex = name.length;
-middleName = Math.ceil(name.length / 2);
+middleName = Math.floor(name.length / 2);
 while (position == -1 && i < name.length - 1) {
     compareResult = nameSearch.localeCompare(name[middleName]);
-    console.log('resultat de comparaison :' + compareResult);
     if (compareResult < 0) {
         hightIndex = middleName;
-        middleName = Math.ceil((hightIndex - lowIndex) / 2);
+        middleName = Math.floor((hightIndex - lowIndex) / 2);
     } else if (compareResult > 0) {
         lowIndex = middleName;
-        middleName += Math.ceil((hightIndex - lowIndex) / 2);
+        middleName += Math.floor((hightIndex - lowIndex) / 2);
     } else {
         position = middleName;
     }
-    console.log("nombre d\'itération :" + i);
     i++;
 }
-console.log(position);
 if (position != -1) {
     console.log("Le nom " + nameSearch + " est en position n° " + position);
 } else {
