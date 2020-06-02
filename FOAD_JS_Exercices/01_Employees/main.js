efbbd0bdc10e02bde7687c63746f85e0a2835239
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-02 17:46:12 +0200

    Update main.js
    Add new class Enterprise test

--- FOAD_JS_Exercices/01_Employees/main.js
+++ FOAD_JS_Exercices/01_Employees/main.js
@@ -6,8 +6,8 @@ let myEnterprise = new Enterprise();
 
 //Création des employees
 let employee1 = new Employee(1, 'Devoldere', 'Mickael', 'manager', 82000, new Date('2015-01-28'));
-let employee2 = new Employee(2, 'Boudier', 'Aurélien', 'Project chief', 41000, new Date('2017-09-16'));
-let employee3 = new Employee(3, 'Shmitt', 'Joanna', 'Déveloper designer', 30000, new Date('2016-05-14'));
+let employee2 = new Employee(3, 'Boudier', 'Aurélien', 'Project chief', 41000, new Date('2017-09-16'));
+let employee3 = new Employee(2, 'Shmitt', 'Joanna', 'Déveloper designer', 30000, new Date('2016-05-14'));
 let employee4 = new Employee(4, 'Hamza', 'Reda', 'Déveloper', 26000, new Date('2015-01-28'));
 let employee5 = new Employee(5, 'Brown', 'Tim', 'Déveloper', 26000, new Date('2018-01-25'));
 
@@ -35,3 +35,21 @@ console.log(myEnterprise.read(5));
 console.log('le plus bas salaire est : ' + lowerSalary.firstname + " " + lowerSalary.lastname);
 console.log('le plus haut salaire est : ' + highterSalary.firstname + " " + highterSalary.lastname);
 console.log('Différence entre le plus haut et le plus bas salaire : ' + differencySalary + " euros");
+
+//Revalorisation salaire
+employee1.salary = 85000;
+myEnterprise.update(employee1);
+employee2.salary = 43000;
+myEnterprise.update(employee2);
+
+//Affichage des employees
+console.log("Affichage des employees aprés modification :");
+console.log(myEnterprise);
+
+//tri des employées par id, salaire
+console.log("**** tri des employées****");
+console.log("By id");
+console.log(myEnterprise.readAll("id"));
+console.log();
+console.log("By hireDate");
+console.log(myEnterprise.readAll("hireDate"));
