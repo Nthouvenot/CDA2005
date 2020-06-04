74f59d40612957223fa344bfade690e0a5384a1a
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-04 11:53:29 +0200

    Update exercice02.js
    Modification de l'algorithme récursif de la multiplication (le résultat s'en raproche mais ne fonctionne toujours pas

--- FOAD_Algo_Recursif/exercice02.js
+++ FOAD_Algo_Recursif/exercice02.js
@@ -2,35 +2,23 @@
 // formule m*n=m+m*(n-1)
 function produit(a, b) {
     if (a == 0) {
-        return a;
-    } else if (b == 0) {
-        return false;
-    } else if (b == 1) {
-        return a;
-    } else if (a > b) {
-        return produit(a += a, b -= 1);
-    }/* else if (a < b) {
-        return produit(a += a, b -= 1);
-    }*/
+        return 0;
+    }
+    if (b == 0) {
+        return a
+    }
+    a -= 1;
+    return produit(a += a, b -= 1);
 }
 
-/*result1 = produit(0, 3); //On teste la division par un numérateur a 0
+result1 = produit(0, 3); //On teste la division par un numérateur a 0
 console.log("Le résultat du produit de 0 et 3 est de " + result1);
 
 result2 = produit(3, 0); //On teste la division par 0
-console.log("Le résultat du produit de 3 et 0 est de " + result2);*/
+console.log("Le résultat du produit de 3 et 0 est de " + result2);
 
 result3 = produit(5, 3); //On teste la division classique a > b
 console.log("Le résultat du produit de 5 et 3 est de " + result3);
-/*
-result4 = produit(3, 5); //On teste la division classique b > a
-console.log("Le résultat du produit de 5 et 3 est de " + result4);*/
 
-/*Solution de départ
-function produit(a, b) {
-    if (b == 0) {
-        return a
-    } else {
-        return produit(a += a, b -= 1);
-    }
-}*/
+result4 = produit(3, 5); //On teste la division classique b > a
+console.log("Le résultat du produit de 3 et 5 est de " + result4);
