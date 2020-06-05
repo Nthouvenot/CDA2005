fbb0d669c0133d10b9b0300b83141bb256e2f432
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-05 12:25:24 +0200

    Create main.js
    Premier test de la class Area

---
+++ FOAD_JS_Exercices/02_Areas/main.js
@@ -0,0 +1,20 @@
+
+const Point = require('./02_Areas_Point.js');
+const Area = require('./02_Areas_part2.js');
+
+//On créer une zone de 6 * 6
+let area1 = new Area(6, 6);
+//on Créer les point de la zone1
+let point1 = new Point(1, 5);
+let point2 = new Point(3, 2);
+let point3 = new Point(1, 6);
+let point4 = new Point(4, 3);
+//On ajoute les points a la zone1
+area1.addPoint(point1);
+area1.addPoint(point2);
+area1.addPoint(point3);
+area1.addPoint(point4);
+
+
+
+console.log(area1);
