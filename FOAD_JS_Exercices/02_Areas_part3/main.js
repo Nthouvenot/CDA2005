6e5b90d39576c01a1e814be9088dc37409d54463
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-16 09:09:29 +0200

    Update main.js
    Update des test des classe dérivées de Point

--- FOAD_JS_Exercices/02_Areas_part3/main.js
+++ FOAD_JS_Exercices/02_Areas_part3/main.js
@@ -12,17 +12,17 @@ let path1 = new Path(3, 5);
 let wall1 = new Wall(6, 2, "marble");
 
 //Test des classe hérité de Point
-console.log("Type de door1 (Point):");
+console.log("Type de door1, Point ?:");
 console.log(door1 instanceof Point);
-console.log("Type de wall1 (Wall):");
-console.log(door1 instanceof Wall);
-
 console.log();
-console.log("Type de goal1 :");
+console.log("Type de wall1, Wall ?:");
+console.log(door1 instanceof Wall);
+console.log();
+console.log("Type de goal1, Point ?:");
 console.log(goal1 instanceof Point);
 console.log();
-console.log("Type de path1 :");
+console.log("Type de path1, Point ?:");
 console.log(path1 instanceof Point);
 console.log();
-console.log("Type de wall1 :");
+console.log("Type de wall1, Point ?:");
 console.log(wall1 instanceof Point);
