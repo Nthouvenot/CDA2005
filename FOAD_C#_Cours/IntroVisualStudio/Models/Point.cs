00ec33822adafea6092eb797c410a6355654cbaf
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-06-29 17:28:00 +0200

    Create IntroVisualStudio
    Premier cours C# avec l'IDE Visual Studio Community 2019

---
+++ FOAD_C#_Cours/IntroVisualStudio/Models/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroVisualStudio.Models
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+        /// <summary>
+        /// constructor no param
+        /// </summary>
+        public Point():this(0, 0)
+        {
+
+        }
+
+        /// <summary>
+        /// constructor one param string
+        /// </summary>
+        /// <param name="_x"></param>
+        public Point(int _x) : this(_x, 0)
+        {
+
+        }
+
+        /// <summary>
+        /// constructor with two integer param
+        /// </summary>
+        /// <param name="_x"></param>
+        /// <param name="_y"></param>
+        public Point(int _x, int _y)
+        {
+            this.x = _x;
+            this.y = _y;
+        }
+    }
+}
