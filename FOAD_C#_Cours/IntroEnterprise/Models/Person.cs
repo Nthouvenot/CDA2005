e99e87ddb55e28a636351b9bf2ffd7f11b817414
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-08 12:27:33 +0200

    Create FOAD_C#_Cours/IntroEnterprise
    Cours sur le polymophisme  et sur les proprieté en C# projet Enterprise

---
+++ FOAD_C#_Cours/IntroEnterprise/Models/Person.cs
@@ -0,0 +1,22 @@
+namespace IntroEnterprise
+{
+    abstract class Person
+    {
+        private string lastname;
+        private string firstname;
+
+        /// <summary>
+        /// contructor with all the attribute in parameters
+        /// </summary>
+        /// <param name="_firstname"></param>
+        /// <param name="_lastname"></param>
+        public Person(string _firstname, string _lastname)
+        {
+            this.firstname = _firstname;
+            this.lastname = _lastname;
+        }
+
+        abstract public string GetInfo();
+       
+    }
+}
