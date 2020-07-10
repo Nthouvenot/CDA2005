0cd6bd9bde760a6cd6639bb5717f22c74abf9210
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-10 18:25:58 +0200

    Update FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
    implémentation de test des constructeurs de la class BankAccount

--- FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
+++ FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
@@ -6,7 +6,12 @@ namespace ExercicePOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            BankAccount c = new BankAccount();
+            BankAccount c1 = new BankAccount(12345, "toto", 1000, -500);
+            Console.WriteLine(c.Print());
+            Console.WriteLine(c1.Print());
         }
     }
 }
