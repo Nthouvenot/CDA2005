8e8bb3426da445534a7292e39cb2041311d001d6
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-15 10:03:54 +0200

    Update FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
    update du au remplacement méthode print par override de ToString (BankAcount)

--- FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
+++ FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
@@ -9,14 +9,14 @@ namespace ExercicePOO
            // first stage
             BankAccount c = new BankAccount();
             BankAccount c1 = new BankAccount(12345, "toto", 1000, -500);
-            Console.WriteLine(c.Print());
-            Console.WriteLine(c1.Print());
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(c1.ToString());
             //second stage
             BankAccount b = new BankAccount(545454, "laurent", 2000, 500);
             b.Credit(100);
             Console.WriteLine(b);
             bool ok = b.Debit(100000);
-            Console.WriteLine(b.Print());
+            Console.WriteLine(b.ToString());
             if(ok)
             {
                 Console.WriteLine("Débit réussi !");
