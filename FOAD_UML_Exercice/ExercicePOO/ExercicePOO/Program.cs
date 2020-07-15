641f8114d71731101f6a3bec9b06a246770e8df2
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-15 11:46:23 +0200

    Update FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
    ajout de séparateurs 00entre les différentes phase de l'exercice

--- FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
+++ FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
@@ -7,12 +7,14 @@ namespace ExercicePOO
     {
         static void Main(string[] args)
         {
-           // first stage
+            // first stage
+            Console.WriteLine("Stage 1 :");
             BankAccount c = new BankAccount();
             BankAccount c1 = new BankAccount(12345, "toto", 1000, -500);
             Console.WriteLine(c.ToString());
             Console.WriteLine(c1.ToString());
             //second stage
+            Console.WriteLine("Stage 2 :");
             BankAccount b = new BankAccount(545454, "laurent", 2000, 500);
             b.Credit(100);
             Console.WriteLine(b);
@@ -27,6 +29,7 @@ namespace ExercicePOO
                 Console.WriteLine("Débit pas réussi !");
             }
             //third stage
+            Console.WriteLine("Stage 3");
             BankAccount c2 = new BankAccount(45657, "titi", 2000, -1000);
             c1.Transfert(1300, c2);
             Console.WriteLine(c1.ToString());
