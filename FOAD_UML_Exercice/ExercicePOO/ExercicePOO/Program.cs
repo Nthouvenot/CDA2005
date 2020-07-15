8ac66044c304303694edf9ca10d07b13120af9d8
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-15 09:55:01 +0200

    Update FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
    implémentation de test des méthode debit et credit de la class BankAccount

--- FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
+++ FOAD_UML_Exercice/ExercicePOO/ExercicePOO/Program.cs
@@ -6,12 +6,25 @@ namespace ExercicePOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-
+           // first stage
             BankAccount c = new BankAccount();
             BankAccount c1 = new BankAccount(12345, "toto", 1000, -500);
             Console.WriteLine(c.Print());
             Console.WriteLine(c1.Print());
+            //second stage
+            BankAccount b = new BankAccount(545454, "laurent", 2000, 500);
+            b.Credit(100);
+            Console.WriteLine(b);
+            bool ok = b.Debit(100000);
+            Console.WriteLine(b.Print());
+            if(ok)
+            {
+                Console.WriteLine("Débit réussi !");
+            }
+            else
+            {
+                Console.WriteLine("Débit pas réussi !");
+            }
         }
     }
 }
