114c241725cbce4f0e763dcb1b8bdef00a4955ed
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-15 10:28:21 +0200

    Update multiple files
    erreur dossier

---
+++ FOAD_UML_Exercice/ExercicePOO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercicePOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // first stage
+            BankAccount c = new BankAccount();
+            BankAccount c1 = new BankAccount(12345, "toto", 1000, -500);
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(c1.ToString());
+            //second stage
+            BankAccount b = new BankAccount(545454, "laurent", 2000, 500);
+            b.Credit(100);
+            Console.WriteLine(b);
+            bool ok = b.Debit(100000);
+            Console.WriteLine(b.ToString());
+            if(ok)
+            {
+                Console.WriteLine("Débit réussi !");
+            }
+            else
+            {
+                Console.WriteLine("Débit pas réussi !");
+            }
+        }
+    }
+}
