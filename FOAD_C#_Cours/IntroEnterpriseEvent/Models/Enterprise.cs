2c9ab5d04e9331d5998ef1fbd1b475f997349ddf
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-31 17:57:41 +0200

    Create FOAD_C#_Cours/IntroEnterpriseEvent/
    Cours C# avec le framework Dot Net Core sur les evenements

---
+++ FOAD_C#_Cours/IntroEnterpriseEvent/Models/Enterprise.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace IntroEnterprise.Models
+{
+    class Enterprise
+    {
+        private string name;
+        private List<Person>employees;
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// contructor with the name attribute in parameter
+        /// </summary>
+        /// <param name="_name"></param>
+        public Enterprise(string _name)
+        {
+            this.name = _name;
+            employees = new List<Person>();
+            //employees[0] = new Client("", "", 1);
+        }
+
+        /// <summary>
+        /// Create the name property
+        /// </summary>
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnProperyChanged("Name");
+            }
+        }
+
+        public void OnProperyChanged(string _propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(_propertyName));
+        }
+
+        /// <summary>
+        /// create the List<Person>employee property
+        /// </summary>
+        internal List<Person> Employees { get => employees; }
+       
+        /// <summary>
+        /// add a new employee at the collection
+        /// </summary>
+        /// <param name="_employee"></param>
+        public void CreateEmployees(Employee _employee)
+        {
+            this.employees.Add(_employee);
+
+        }
+
+        public int CalculatePrime(ISalary _emp)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// calculate the bonus of the employee
+        /// </summary>
+        /// <param name="_employee"></param>
+        /// <returns>int</returns>
+        public int CalculateBonus(Employee _employee)
+        {
+            Employee p = (Employee)_employee;
+            return 0;
+        }
+
+    }
+
+}
---
+++ FOAD_C#_Cours/IntroEnterpriseEvent/Program.cs
@@ -0,0 +1,44 @@
+﻿using IntroEnterprise.Models;
+using System;
+
+namespace IntroEnterprise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Enterprise ets = new Enterprise("CRM");
+            Client c = new Client("DEV", "mike", 1);
+            Employee employee = new Employee("John", "Doe");
+            ets.CreateEmployees(employee);
+            ets.PropertyChanged += MyEvent;
+            Console.WriteLine(ets.Employees);
+
+            int i;
+            for(i=0; i < ets.Employees.Count; i++)
+            {
+
+            }
+
+            foreach(Employee item in ets.Employees)
+            {
+                Console.WriteLine(item);
+            }
+
+            ets.Name = "Doe";
+            ets.Name = "hoe";
+            ets.Name = "a";
+        }
+
+        static void MyEvent(object Sender, EventArgs e)
+        {
+            Enterprise ets = Sender as Enterprise;
+            if(ets == null)
+            {
+                return;
+            }
+            Console.WriteLine("Le nom de l'entreprise a changé " + ets.Name);
+        }
+    }
+}
