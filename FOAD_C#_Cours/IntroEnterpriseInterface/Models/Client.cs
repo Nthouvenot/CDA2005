6617aad5b2b7e784e900ed83090431f9b3f60085
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-07-31 17:58:43 +0200

    Create FOAD_C#_Cours/IntroEnterpriseInterface/
    Cours C# avec le framework Dot Net Core sur les interfaces

---
+++ FOAD_C#_Cours/IntroEnterpriseInterface/Models/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroEnterprise.Models
+{
+    class Client : Person
+    {
+        int idClient;
+
+        /// <summary>
+        /// constructor with the idClient attribute in parameter and call to the constructor of the base class
+        /// </summary>
+        /// <param name="_firstname"></param>
+        /// <param name="_lastname"></param>
+        /// <param name="_idClient"></param>
+        public Client(string _firstname, string _lastname, int _idClient) : base(_firstname, _lastname)
+        {
+            this.idClient = _idClient;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string GetInfo()
+        {
+            return "";
+        }
+
+
+    }
+}
