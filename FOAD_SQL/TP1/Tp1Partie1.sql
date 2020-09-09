a2ce1c0b9d9fed3589872b74ce23d4a549898123
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-09-09 18:31:56 +0200

    Rename file and Update
    premiere partie du TP1 du DML MySql toute les question sont faite sauf la 15

--- FOAD_SQL/TP1/Tp1Partie1.sql
+++ FOAD_SQL/TP1/Tp1Partie1.sql
@@ -41,4 +41,15 @@ SELECT ENAME, JOB, DATE_FORMAT(HIREDATE, '%a\ %d\ %b\ %Y') AS HIREDATE2 FROM emp
 SELECT DNAME, MAX(SAL) AS SALAIRE_MAX FROM emp INNER JOIN dept ON emp.DEPTNO = dept.DEPTNO GROUP BY DNAME ORDER BY DNAME ASC;
 
 -- 14. Donner département par département masse salariale, nombre d'employés, salaire moyen par type d'emploi.
-SELECT (SUM(SAL) + SUM(COMM)) AS payroll, COUNT(ENAME) AS numbre_of_employees, AVG(SAL) AS salary_average, JOB FROM emp GROUP BY JOB ORDER BY JOB ASC;
+SELECT SUM(SAL + CASE WHEN COMM IS NULL THEN 0 ELSE COMM END) AS payroll, COUNT(ENAME) AS numbre_of_employees, AVG(SAL) AS salary_average, JOB, DNAME FROM emp INNER JOIN dept ON emp.DEPTNO = dept.DEPTNO GROUP BY JOB ORDER BY JOB ASC;
+-- version Franck
+select sum(sal + CASE WHEN comm IS NULL THEN 0 ELSE comm END), count(ename), avg(sal) from emp where ename < 2 group by deptno ORDER BY JOB ASC;
+
+-- 15. Même question mais on se limite aux sous-ensembles d'au moins 2 employés
+SELECT SUM(SAL + CASE WHEN COMM IS NULL THEN 0 ELSE COMM END) AS payroll, COUNT(ENAME) AS numbre_of_employees, AVG(SAL) AS salary_average, JOB, DNAME FROM emp INNER JOIN dept ON emp.DEPTNO = dept.DEPTNO GROUP BY JOB ORDER BY JOB ASC; -- reste a faire
+
+-- 16. Liste des employés (Nom, département, salaire) de même emploi que JONES
+SELECT ENAME, JOB, DNAME, SAL FROM emp INNER JOIN dept ON emp.DEPTNO = dept.DEPTNO WHERE JOB = (SELECT JOB FROM emp WHERE ENAME='JONES');
+
+-- 17. Liste des employés (nom, salaire) dont le salaire est supérieur à la moyenne globale des salaires
+SELECT ENAME, SAL FROM emp HAVING SAL > AVG(SAL); -- avg ne fonctionne pas avec having
