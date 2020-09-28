7f5892a2049a335cc494242266650cd839ac0713
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-09-28 09:54:19 +0200

    Update employe.php
    remplacement require par require_once, redefinition du destructeur pour différencier la destruction d'un objet de type personne et d'un employe

--- FOAD_Web/PHP/InitiationObjets/employe.php
+++ FOAD_Web/PHP/InitiationObjets/employe.php
@@ -1,31 +1,35 @@
 <?php 
     namespace InitiationObjets;
-    require './personne.php';
+    require_once './personne.php';
     class employe extends personne {
         private int $_numEmp;
         
-        function __construct($nom, $prenom, $email, $adresse, $numEmp) {
+        public function __construct($nom, $prenom, $email, $adresse) {
             parent::__construct($nom, $prenom, $email, $adresse);
-            $this->_numEmp = $numEmp;
+            $this->_numEmp = 0;
         }
         
-        function GetEmploye() {
+        public function GetEmploye() {
             echo $this->GetPersonne().' numero employé: '.$this->_numEmp;
         }
         
-        function GetAdresse() {
+        public function GetAdresse() {
             return $this->_adresse;
         }
         
         /* nous avons pas besoins de réecrire la méthode SetAdresse elle est disponible directement dans la classe employee car
         celle-ci herite de personne **/
         
-        function GetNum() {
+        public function GetNum() {
             return $this->_numEmp;
         }
         
-        function SetNum($numEmp) {
+        public function SetNum($numEmp) {
             $this->_numEmp = $numEmp;
+        }
+        
+        public function __destruct() {
+            echo 'L\'objet de type employe a bien été détruit<br>';
         }
     }
 
