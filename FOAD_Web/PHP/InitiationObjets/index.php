080257b5a1fd8fd9787e10a2bda3e521aa3b53cc
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-09-28 09:55:32 +0200

    Update index.php
    remplacement require par require_once + correction d'une erreur d'appel de méthode

--- FOAD_Web/PHP/InitiationObjets/index.php
+++ FOAD_Web/PHP/InitiationObjets/index.php
@@ -1,6 +1,7 @@
 <?php
     namespace InitiationObjets;
-    require('./personne.php');
+    require_once ('./personne.php');
+    require_once ('./employe.php');
 ?>
 <!DOCTYPE HTML>
 <html lang="fr">
@@ -13,12 +14,22 @@
 		<p>
 		<?php 
 		
-		    $personne1 = new Personne('nicolas', 'thouvenot','mon_mail@mail.com', 'mon_adresse');
+		    $personne1 = new personne('nicolas', 'thouvenot','mon_mail@mail.com', 'mon_adresse');
 		    $personne1->SetEmail('nicolas.thouvenot@mail.com');
 		    $personne1->SetAdresse('2 Rue General de Gaulle 52630 Grandchamp');
-		    echo 'donnée de personne 1 :'.$personne1->GetPersonne().'<br>';
+		    echo '<p>donnée de personne 1 :'.$personne1->GetPersonne().'<br>';
 		    echo 'affichage de l\'objet personne1 :<br>';
 		    var_dump($personne1);
+		    echo '</p>';
+		    
+		    $employe1 = new employe('mickael', 'devoldére','mon_mail@mail.com', 'mon_adresse');
+		    $employe1->SetEmail('mdevoldere@arfp.asso.fr');
+		    $employe1->SetAdresse('57 rue Albert-Camu 68200 Mulhouse');
+		    $employe1->SetNum(1540);
+		    echo '<p>donnée de employe 1 :'.$employe1->GetEmploye().'<br>';
+		    echo 'affichage de l\'objet personne1 :<br>';
+		    var_dump($employe1);
+		    echo '</p>';
         ?>
         </p>
 	</body>
