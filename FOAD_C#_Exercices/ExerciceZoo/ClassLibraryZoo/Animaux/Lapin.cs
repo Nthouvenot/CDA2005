75961c9a0114af011f4e7ca4346831fccc95fb1f
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-10-08 18:15:21 +0200

    Create ExerciceZoo
    implementation sort des animaux par l'age, implementation Interface IDeplacable et Iparleable + program terminer

---
+++ FOAD_C#_Exercices/ExerciceZoo/ClassLibraryZoo/Animaux/Lapin.cs
@@ -0,0 +1,44 @@
+﻿using ClassLibraryZoo.Contrats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryZoo.Animaux
+{
+    public class Lapin : AnimalDuZoo
+    {
+        int lgOreilleEnCm;
+
+        public Lapin():base()
+        {
+            this.lgOreilleEnCm = 10;
+        }
+
+        public int LgOreilleEnCm 
+        {
+            get => this.lgOreilleEnCm;
+            set
+            {
+                if(value > 20)
+                {
+                    return;
+                }
+                this.lgOreilleEnCm = value;
+            }
+        }
+
+        public void Manger()
+        {
+            Console.WriteLine("Je mange des carrotes");
+            return;
+        }
+
+        public override bool SeDeplacer()
+        {
+            Console.WriteLine("Je saute");
+            return true;
+        }
+    }
+}
---
+++ FOAD_C#_Exercices/ExerciceZoo/ConsoleAppTestZoo/Program.cs
@@ -0,0 +1,63 @@
+﻿using ClassLibraryZoo.Animaux;
+using ClassLibraryZoo.Employes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestZoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool secoursAppelle = false;
+            List<AnimalDuZoo> mesAnimauxDuZoo = new List<AnimalDuZoo>();
+            mesAnimauxDuZoo.Add(new Lapin());
+            mesAnimauxDuZoo.Add(new Lapin());
+            mesAnimauxDuZoo.Add(new Lion());
+            mesAnimauxDuZoo.Add(new Perroquet());
+            List<EmployeesDuZoo> mesEmployeesDuZoo = new List<EmployeesDuZoo>();
+            mesEmployeesDuZoo.Add(new Gardien(DateTime.Now, false));
+            mesEmployeesDuZoo.Add(new Gardien(DateTime.Now, false));
+
+            //il faut mettre un peu d'ordre dans l'evacuation...du plus vieux au plus jeune
+            mesAnimauxDuZoo.Sort();
+
+            //le feu arrive, il faut deplacer tout le monde
+            Console.WriteLine("Au feu, au feu, il y a le zoo qui crame !!!");
+            foreach (AnimalDuZoo animal in mesAnimauxDuZoo)
+            {
+                animal.SeDeplacer();
+            }
+
+            //mais que fait le gardien? il reste sur place?
+            foreach (EmployeesDuZoo employe in mesEmployeesDuZoo)
+            {
+                employe.SeDeplacer();
+            }
+
+            //et qui va appeller les secours? 
+            foreach (EmployeesDuZoo employe in mesEmployeesDuZoo)
+            {
+                if (employe.EstEnPoste && !secoursAppelle)
+                {
+                    employe.AppellerSecours();
+                    secoursAppelle = true;
+                }
+                else
+                {
+                    secoursAppelle = false;
+                }
+            }
+            if (!secoursAppelle)
+            {
+                Perroquet p = (Perroquet)mesAnimauxDuZoo.Find(animal => animal.GetType() == typeof(Perroquet)); //
+                p.AppellerSecours();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
