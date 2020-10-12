3054a7e8d67837e568a84552454c721ebbaa19f2
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-10-12 16:35:35 +0200

    Create ExrciceForet
    exercice sur les enumérationsn sucharge de methode ToString

---
+++ FOAD_C#_Exercices/ExerciceForet/ClassLibraryForet/Feuille.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForet
+{
+    public class Feuille
+    {
+        private int nbNervures;
+        private EnumCouleur couleurFeuille;
+        private EnumFormeFeuille formeFeuille;
+
+        public Feuille(int nbNervures)
+        {
+            this.nbNervures = nbNervures;
+        }
+
+        public Feuille(int nbNervures, EnumFormeFeuille formeFeuille)
+        {
+            this.nbNervures = nbNervures;
+            this.formeFeuille = formeFeuille;
+            this.couleurFeuille = EnumCouleur.Vert;
+        }
+
+        public Feuille(int nbNervure, EnumFormeFeuille formeFeuille, EnumCouleur couleurFeuille)
+        {
+            this.nbNervures = nbNervure;
+            this.formeFeuille = formeFeuille;
+            this.couleurFeuille = couleurFeuille;
+
+        }
+
+        public int NbNervures 
+        { 
+            get => nbNervures; 
+          //set => nbNervures = value; 
+        }
+
+        public void Tomber()
+        {
+            Console.WriteLine("Je tombe");
+        }
+
+        public void PasserEnAutomne()
+        {
+            if(this.couleurFeuille == EnumCouleur.Jaune)
+            {
+                this.couleurFeuille = EnumCouleur.Rouge;
+            }
+            this.couleurFeuille = EnumCouleur.Jaune;
+        }
+
+        public override string ToString()
+        {
+            return "nombre de nervure : " + this.nbNervures + ", forme de feuille : " + this.formeFeuille  + ", couleur : " + this.couleurFeuille;
+        }
+    }
+}
