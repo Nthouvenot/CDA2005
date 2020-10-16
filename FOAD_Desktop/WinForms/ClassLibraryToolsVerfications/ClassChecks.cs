c23c11edd6f34cf134c9d258a67dfde22cc7cbc0
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-10-16 12:18:13 +0200

    Create ValidationSaisies, ClassLibrairyFacture, ClassLibraryToolsVerfications
    exercice pahase 2 validation de la saisie a terminer verification date + MessageBox fermeture application

---
+++ FOAD_Desktop/WinForms/ClassLibraryToolsVerfications/ClassChecks.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibraryToolsChecks
+{
+    public static class ClassChecks    {
+        public static bool CheckName(string _name)
+        {
+            Regex regexName = new Regex(@"^[a-zA-Z]{4,30}$");
+            if (!regexName.IsMatch(_name))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckDate(string _date)
+        {
+           // ToDo accpet multiple format use DateTime object => DateTime.ParseExact
+            if(_date.Length != 10)
+            {
+                return false;
+            }
+            DateTime billDate = Convert.ToDateTime(_date);
+            if(!(billDate < DateTime.Now))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckAmount(string _amount)
+        {
+            Regex regexAmount = new Regex(@"^[0-9]{1,4}\.?,?[0-9]{1,2}$");
+            if(!regexAmount.IsMatch(_amount))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckZipCode(string _zipCode)
+        {
+            Regex regexName = new Regex(@"^[0-9]{5}$");
+            if (!regexName.IsMatch(_zipCode))
+            {
+                return false;
+            }
+            else if(Int32.Parse(_zipCode) > 96000)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ FOAD_Desktop/WinForms/ValidationSaisies/Form1.cs
@@ -0,0 +1,100 @@
+﻿using ClassLibraryToolsChecks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidationSaisies
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Generic methode for validate the content of the TextBoxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            TextBox input = (TextBox)sender;
+            switch(input.Name)
+            {
+                case "textBoxName":
+                    {
+                        if (!ClassChecks.CheckName(input.Text))
+                        {
+                            errorProviderNom.SetError(input, "Le nom doit contenir de 2 a 30 lettres");
+                        } else
+                        {
+                            errorProviderNom.Clear();
+                        }
+                        
+                        break;
+                    }
+                case "textBoxDate":
+                    {
+                        if (!ClassChecks.CheckDate(input.Text))
+                        {
+                            errorProviderDate.SetError(input, "La date doit être au fomat date JJ/MM/YYYY");
+                        } else
+                        {
+                            errorProviderDate.Clear();
+                        }
+
+                        break;
+                    }
+                case "textBoxAmount":
+                    {
+                        if(!ClassChecks.CheckAmount(input.Text))
+                        {
+                            errorProviderAmount.SetError(input, "Le montant doit être au format decimal avec deux chiffre aprés la virgule");
+                        } else
+                        {
+                            errorProviderAmount.Clear();
+                        }
+                        break;
+                    }
+                case "textBoxZipCode":
+                    {
+                        if(!ClassChecks.CheckZipCode(input.Text))
+                        {
+                            errorProviderZipCode.SetError(input, "Le code postal doit contenir 5 chiffres et être inférieur a 96000");
+                        } else
+                        {
+                            errorProviderZipCode.Clear();
+                        }
+                        break;
+                    }
+            }
+        }
+
+        private void validate_Click(object sender, EventArgs e)
+        {
+            string message = "Nom : " +  textBoxName.Text + "\n" + "Date : " + textBoxDate.Text + "\n" + "Montant : " + textBoxAmount.Text + "\n" + "Code : " + textBoxZipCode.Text;
+            MessageBox.Show(message, "Validation saisie", MessageBoxButtons.OK);
+
+        }
+        /// <summary>
+        /// Clear all the TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBoxName.Clear();
+            textBoxDate.Clear();
+            textBoxAmount.Clear();
+            textBoxZipCode.Clear();
+        }
+    }
+}
