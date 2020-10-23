6ba1867d855934f2bce7ca24ad0e1e3f20ba1a36
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-10-23 12:09:25 +0200

    Create ListBox
    demarrage de l'exercice Winforms sur les ListBox design interface + errorProvider + Classe bibliotheque de vérification entree faite

---
+++ FOAD_Desktop/WinForms/ListBox/ClassLibraryCheckTools/ClassCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCheckTools
+{
+    public class ClassCheck
+    {
+        public static bool CheckName(string _name)
+        {
+            Regex regexName = new Regex(@"^[a-zA-Z]{2,15}$");
+            if (!regexName.IsMatch(_name))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckIndex(string _name)
+        {
+            Regex regexName = new Regex(@"^[0-9]{1,4}$");
+            if (!regexName.IsMatch(_name))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ FOAD_Desktop/WinForms/ListBox/ListBoxMain/Form1.cs
@@ -0,0 +1,45 @@
+﻿using ClassLibraryCheckTools;
+using System;
+using System.Windows.Forms;
+
+namespace ListBox
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            TextBox name = (TextBox)sender;
+            if (!ClassCheck.CheckName(name.Text))
+            {
+                errorProviderName.SetError(name, "Un nom comporte entre 2 et 15 lettres");
+            } else
+            {
+                errorProviderName.Clear();
+            }
+        }
+
+        private void textBoxIndex_TextChanged(object sender, EventArgs e)
+        {
+            TextBox name = (TextBox)sender;
+            if (!ClassCheck.CheckIndex(name.Text))
+            {
+                errorProviderIndex.SetError(name, "Un index comporte entre 1 et 4 chiffres");
+            }
+            else
+            {
+                errorProviderIndex.Clear();
+            }
+        }
+
+        private void buttonAddList_Click(object sender, EventArgs e)
+        {
+            //implement listboxName.? <-- textBoxName.Text
+        }
+    }
+}
+
