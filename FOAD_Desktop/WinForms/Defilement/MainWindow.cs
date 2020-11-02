df1c83243a71f91582b830879e5bf714f1ab60e7
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-11-02 12:51:02 +0100

    Create Defilement
    exercice WinForms sur les HScrollBar et les NumericUpDown

---
+++ FOAD_Desktop/WinForms/Defilement/MainWindow.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Defilement
+{
+    public partial class FormDefilement : Form
+    {
+        public FormDefilement()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Methode called when the Value has changed or the user scroll the HScrollBars
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void hScrollBar_ValueChangedOrScrolled(object sender, EventArgs e)
+        {
+            HScrollBar scrollBarSender = (HScrollBar)sender;
+            switch (scrollBarSender.Name)
+            {
+                case "hScrollBarRed":
+                    {
+                        numericUpDownRed.Value = hScrollBarRed.Value;
+                        break;
+                    }
+                case "hScrollBarGreen":
+                    {
+                        numericUpDownGreen.Value = hScrollBarGreen.Value;
+                        break;
+                    }
+                case "hScrollBarBlue":
+                    {
+                        numericUpDownBlue.Value = hScrollBarBlue.Value;
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// When the value of one NumericUpDown has changed we update the background of the NumericUpDown has been changed and the Label labelColorResult background color
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDownSender = (NumericUpDown)sender;
+            switch (numericUpDownSender.Name)
+            {
+                case "numericUpDownRed":
+                    {
+                        hScrollBarRed.Value = (int)numericUpDownSender.Value;
+                        labelPrintRed.BackColor = Color.FromArgb((int)numericUpDownSender.Value, 0, 0);
+                        break;
+                    }
+                case "numericUpDownGreen":
+                    {
+                        hScrollBarGreen.Value = (int)numericUpDownGreen.Value;
+                        labelPrintGreen.BackColor = Color.FromArgb(0, (int)numericUpDownSender.Value, 0);
+                        break;
+                    }
+                case "numericUpDownBlue":
+                    {
+                        hScrollBarBlue.Value = (int)numericUpDownBlue.Value;
+                        labelPrintBlue.BackColor = Color.FromArgb(0, 0, (int)numericUpDownSender.Value);
+                        break;
+                    }
+            }
+            labelColorResult.BackColor = Color.FromArgb((int)numericUpDownRed.Value, (int)numericUpDownGreen.Value, (int)numericUpDownBlue.Value);
+        }
+    }
+}
