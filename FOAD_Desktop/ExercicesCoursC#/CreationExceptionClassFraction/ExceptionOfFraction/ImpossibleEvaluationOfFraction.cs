05b077c8d444e61d8c90d35574619eeeb922e9a7
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-11-16 12:17:45 +0100

    Create CreationExceptionClassFraction
    exercice sur la creation d'exception sur la classe Fraction

---
+++ FOAD_Desktop/ExercicesCoursC#/CreationExceptionClassFraction/ExceptionOfFraction/ImpossibleEvaluationOfFraction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCreationDUneException.ExceptionOfFraction
+{
+    public class ImpossibleEvaluationOfFraction:Exception
+    {
+        public ImpossibleEvaluationOfFraction() : base()
+        {
+            this.HelpLink = "https://docs.microsoft.com";
+            this.Source = "InvalidFractionDenominateurException_Class_Samples";
+        }
+        public ImpossibleEvaluationOfFraction(string message) : base(message)
+        {
+            this.HelpLink = "https://docs.microsoft.com";
+            this.Source = "InvalidFractionDenominateurException_Class_Samples";
+        }
+        public ImpossibleEvaluationOfFraction(string message, System.Exception inner) : base(message, inner)
+        {
+            this.HelpLink = "https://docs.microsoft.com";
+            this.Source = "InvalidFractionDenominateurException_Class_Samples";
+        }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client.
+        protected ImpossibleEvaluationOfFraction(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ FOAD_Desktop/ExercicesCoursC#/CreationExceptionClassFraction/InvalidFractionDenominateurException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCreationDUneException
+{
+    [Serializable()]
+    public class InvalidFractionDenominateurException : System.Exception
+    {
+        public InvalidFractionDenominateurException() : base() 
+        {
+            this.HelpLink = "https://docs.microsoft.com";
+            this.Source = "InvalidFractionDenominateurException_Class_Samples";
+        }
+        public InvalidFractionDenominateurException(string message) : base(message) 
+        { 
+        }
+        public InvalidFractionDenominateurException(string message, System.Exception inner) : base(message, inner) 
+        { 
+        }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client.
+        protected InvalidFractionDenominateurException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
