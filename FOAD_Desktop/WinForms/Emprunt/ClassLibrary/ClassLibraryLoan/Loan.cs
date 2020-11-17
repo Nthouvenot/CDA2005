69d38beb83d9c52c58ca051f1e958dc49a83ea30
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-11-17 17:57:14 +0100

    ajout de propriété pour les champs et utilisation d'une enum pour la durée de remboursement

--- FOAD_Desktop/WinForms/Emprunt/ClassLibrary/ClassLibraryLoan/Loan.cs
+++ FOAD_Desktop/WinForms/Emprunt/ClassLibrary/ClassLibraryLoan/Loan.cs
@@ -8,49 +8,42 @@ namespace ClassLibraryLoan
 {
     public class Loan
     {
-        private int rate;
+        private float rate;
         int numberOfMonth;
         private int amount;
         private float amountPerMonth;
-
-        private enum reimbursementPeriod { monthly = 1, biMonthly = 2, triMonthly = 3, biAnnual = 6, annual = 12 };
+        public enum reimbursementPeriod { monthly = 1, biMonthly = 2, triMonthly = 3, biAnnual = 6, annual = 12 };
+        private reimbursementPeriod currentReimbursementPeriod;
 
         public Loan()
         {
-            this.rate = 3;
+            this.rate = 7;
             this.numberOfMonth = 1;
             this.amount = 0;
             this.amountPerMonth = 0;
-            reimbursementPeriod currentReimbursementPeriod = reimbursementPeriod.monthly;
+            this.currentReimbursementPeriod = reimbursementPeriod.monthly;
         }
 
         /// <summary>
         /// Property of the rate attribute
         /// check if the value is 3, 6 or 9 elseif reject the set
         /// </summary>
-        public int Rate 
-        { 
+        public float Rate
+        {
             get => rate;
-            set
-            {
-                if(value != 7 || value != 8 || value != 9)
-                {
-                    return;
-                }
-                this.rate = value;
-            }
+            set => rate = value;
         }
 
         /// <summary>
         /// Property of the numberOfMonth attribute
         /// check if the value is lower than 25 years
         /// </summary>
-        public int NumberOfMonth 
-        { 
+        public int NumberOfMonth
+        {
             get => numberOfMonth;
             set
             {
-                if(value > (25 * 12))
+                if (value > (25 * 12))
                 {
                     return;
                 }
@@ -59,15 +52,25 @@ namespace ClassLibraryLoan
         }
 
         /// <summary>
+        /// Property of the currentReimbursementPeriod attribute
+        /// Check if the value is superior than the numberOfMonth
+        /// </summary>
+        public reimbursementPeriod CurrentReimbursementPeriod
+        {
+            get => this.currentReimbursementPeriod;
+            set => this.currentReimbursementPeriod = value;
+        }
+
+        /// <summary>
         /// Property of the amount attribute
         /// Check if the value is lower than the maximum capacity of an integer 
         /// </summary>
-        public int Amount 
+        public int Amount
         {
-            get => amount; 
+            get => amount;
             set
             {
-                if(value > Int32.MaxValue)
+                if (value > Int32.MaxValue)
                 {
                     return;
                 }
@@ -81,9 +84,18 @@ namespace ClassLibraryLoan
         /// <returns></returns>
         public float Calculate()
         {
-            float ratePerMonth = rate / 12;
-            this.amountPerMonth = (float)(amount * (ratePerMonth / (1 - (1 + ratePerMonth) * Math.Exp(-this.numberOfMonth))));
+            float ratePerMonth = (rate / 1200) * (int)this.currentReimbursementPeriod;
+            this.amountPerMonth = (float)(amount * (ratePerMonth / (1 - Math.Pow((1 + ratePerMonth), -this.numberOfMonth))));
             return amountPerMonth;
+        }
+
+        /// <summary>
+        /// calculate the total number of month
+        /// </summary>
+        /// <returns></returns>
+        public int CalculateNumberOfMonth()
+        {
+            return this.numberOfMonth / (int)this.currentReimbursementPeriod;
         }
     }
 }
