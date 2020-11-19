ce0a57fa139778febc54414c580624b319a6f188
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-11-19 18:38:38 +0100

    Create ExerciceMeteo
    exercice sur les evenement classe Meteo

---
+++ FOAD_Desktop/ExercicesCoursC#/CreationEvenementNonGraphique/ExerciceMeteo/ClassLibraryMeteo/Temperature.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTemp
+{
+    public class Temperature
+    {
+        private float temperatureCourante;
+        private float[] temperatureDAlertes;
+
+        //Declaration des delegates
+        public delegate void DelegateGel(Temperature sender);
+        public delegate void DelegateFievre(Temperature sender);
+        public delegate void DelegateAlerte(Temperature sender);
+
+        //Declaration des evenements
+        public event DelegateGel Gel;
+        public event DelegateFievre Fievre;
+        public event DelegateAlerte Alerte;
+
+        /// <summary>
+        /// On initialise la température au zero absolu
+        /// </summary>
+        public Temperature()
+        {
+            this.temperatureCourante = -273.15f;
+        }
+
+        public float TemperatureCourante
+        {
+            get => temperatureCourante;
+            //set => temperature = value;
+        }
+
+        /// <summary>
+        /// evenement qui se produit qd risque de gel 0 --- Gel
+        /// evenement qui se produit qd on a de la fievre 38--- Fievre
+        /// 
+        /// 
+        /// Cerise sur le gateau:
+        /// 
+        /// evenement preconfigurable...evenement declenche pour une temperature specifique
+        /// </summary>
+
+
+
+        /// <summary>
+        /// Augmente la température courante si elle n'est pas au dessous dessu de la temperature de la surface du soleil
+        /// </summary>
+        /// <param name="temperature"></param>
+        public void Monter(float temperature)
+        {
+            if (temperature > 5526f && temperature > this.temperatureCourante)
+            {
+                return;
+            }
+            this.temperatureCourante = temperature;
+            if (temperature >= 38 && Fievre != null)
+            {
+                this.Fievre(this);
+            }
+            //else if (temperatureDAlertes.Contains(temperature) && Alerte != null)
+            //{
+            //    this.Alerte(this);
+            //}
+        }
+
+        /// <summary>
+        /// Diminue la température courante si elle n'est pas en dessous du zero absolu
+        /// </summary>
+        /// <param name="_temperature"></param>
+        public void Diminuer(float temperature)
+        {
+            if (temperature < -273.15f && temperature < this.TemperatureCourante)
+            {
+                return;
+            }
+            this.temperatureCourante = temperature;
+            if (temperature == 0 && Gel != null)
+            {
+                this.Gel(this);
+            }
+            //else if (temperatureDAlertes.Contains(temperature) && Alerte != null)
+            //{
+            //    this.Alerte(this);
+            //}
+        }
+    }
+}
---
+++ FOAD_Desktop/ExercicesCoursC#/CreationEvenementNonGraphique/ExerciceMeteo/ConsoleAppHumain/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryTemp;
+
+namespace ConsoleAppHumain
+{
+    class Program
+    {
+        /// <summary>
+        /// Methode pour l'evenement Fievre
+        /// </summary>
+        /// <param name="sender"></param>
+        private static void JaiDeLaFievre(Temperature sender)
+        {
+            Console.WriteLine("Aie Aie Aie j'ai de la fiévre, j'ai " + sender.TemperatureCourante);
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            //declencher rdv chez le medecin si l'evenement Fievre arrive
+            Temperature temperatureHumaine = new Temperature();
+            temperatureHumaine.Fievre += JaiDeLaFievre;
+
+            temperatureHumaine.Monter(37); //pas de Fievre
+            Console.WriteLine("j'ai " + temperatureHumaine.TemperatureCourante);
+            temperatureHumaine.Monter(38.5f); //l'evenement Fievre se declenche
+            Console.WriteLine("j'ai " + temperatureHumaine.TemperatureCourante);
+            Console.ReadLine(); //On fait une pause pour le resultat de l'application
+        }
+
+        
+    }
+}
