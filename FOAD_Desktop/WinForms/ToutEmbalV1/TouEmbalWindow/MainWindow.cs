9751fe1b88aea061bc12902c1b77c510f1cd46be
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-12-04 10:46:06 +0100

    FOAD_Desktop/WinForms/ToutEmbalV1/TouEmbalWindow/MainWindow.cs
    implementation de l'interaction de la class WoodBoxProduction avec l'ihm

--- FOAD_Desktop/WinForms/ToutEmbalV1/TouEmbalWindow/MainWindow.cs
+++ FOAD_Desktop/WinForms/ToutEmbalV1/TouEmbalWindow/MainWindow.cs
@@ -7,14 +7,262 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClassLibraryToutEmbal;
 
 namespace TouEmbalWindow
 {
     public partial class MainWindow : Form
     {
+        private WoodBoxProduction[] woodBoxProductions;
+
+        public delegate void UpdateIHMDelegate(WoodBoxProduction production);
+
         public MainWindow()
         {
             InitializeComponent();
+            this.woodBoxProductions = new WoodBoxProduction[3];
+            this.woodBoxProductions[0] = new WoodBoxProduction(10000, 277,  "Type A");
+            this.woodBoxProductions[0].WoodBoxNumberOfBoxProductedChanged += this.woodBoxNumberOfBoxProductedChanged;
+            this.woodBoxProductions[0].WoodBoxProductiontStateChanged += this.woodBoxProductiontStateChanged;
+            this.woodBoxProductions[1] = new WoodBoxProduction(25000, 55, "Type B");
+            this.woodBoxProductions[1].WoodBoxNumberOfBoxProductedChanged += this.woodBoxNumberOfBoxProductedChanged;
+            this.woodBoxProductions[0].WoodBoxProductiontStateChanged += this.woodBoxProductiontStateChanged;
+            this.woodBoxProductions[2] = new WoodBoxProduction(120000, 2, "Type C");
+            this.woodBoxProductions[2].WoodBoxNumberOfBoxProductedChanged += this.woodBoxNumberOfBoxProductedChanged;
+            this.woodBoxProductions[0].WoodBoxProductiontStateChanged += this.woodBoxProductiontStateChanged;
+        }
+
+        /// <summary>
+        /// method for the comunication inter-threads
+        /// </summary>
+        /// <returns></returns>
+        private void UpdateIhmNumberOfBox(WoodBoxProduction production)
+        {
+            int progress = production.CalculatePercentageOfProductionBox() / 5;
+            switch (production.BoxName)
+            {
+                case "Type A":
+                    {
+                        textBoxNumberOfBoxProducedA.Text = production.CalculateNumberOfGoodBox().ToString();
+                        textBoxRateDefaultPerHourA.Text = production.CalculateRateDefaultPerHour().ToString();
+                        textBoxDefaultRateBeginStartA.Text = production.CalculateRateDefaultPerHour().ToString();
+                        progressBarProductionA.Value = progress;
+                        break;
+                    }
+
+                case "Type B":
+                    {
+                        textBoxNumberOfBoxProducedB.Text = production.CalculateNumberOfGoodBox().ToString();
+                        textBoxRateDefaultPerHourB.Text = production.CalculateRateDefaultPerHour().ToString();
+                        textBoxDefaultRateBeginStartB.Text = production.CalculateRateDefaultPerHour().ToString();
+                        progressBarProductionB.Value = progress;
+                        break;
+                    }
+                case "Type C":
+                    {
+                        textBoxNumberOfBoxProducedC.Text = production.CalculateNumberOfGoodBox().ToString();
+                        textBoxRateDefaultPerHourC.Text = production.CalculateRateDefaultPerHour().ToString();
+                        textBoxDefaultRateBeginStartC.Text = production.CalculateRateDefaultPerHour().ToString();
+                        progressBarProductionC.Value = progress;
+                        break;
+                    }
+            }
+            
+        }
+
+        /// <summary>
+        /// start a box production
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItemStart_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)sender;
+            switch (menuItem.Name)
+            {
+                case "toolStripMenuItemStartA":
+                    {
+                        woodBoxProductions[0].StartProduction();
+                        toolStripMenuItemStartA.Enabled = false;
+                        toolStripMenuItemStopA.Enabled = true;
+                        toolStripMenuItemContinueA.Enabled = false;
+                        tabControlType.SelectedTab = tabPageTypeA;
+                        break;
+                    }
+                case "toolStripMenuItemStartB":
+                    {
+                        woodBoxProductions[1].StartProduction();
+                        toolStripMenuItemStartB.Enabled = false;
+                        toolStripMenuItemStopB.Enabled = true;
+                        toolStripMenuItemContinueB.Enabled = false;
+                        tabControlType.SelectedTab = tabPageTypeB;
+                        break;
+                    }
+                case "toolStripMenuItemStartC":
+                    {
+                        woodBoxProductions[2].StartProduction();
+                        toolStripMenuItemStartC.Enabled = false;
+                        toolStripMenuItemStopC.Enabled = true;
+                        toolStripMenuItemContinueC.Enabled = false;
+                        tabControlType.SelectedTab = tabPageTypeC; 
+                        break;
+                    }
+            }
+        }
+        /// <summary>
+        /// make a break of the production
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItemStop_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)sender;
+            switch (menuItem.Name)
+            {
+                case "toolStripMenuItemStopA":
+                    {
+                        woodBoxProductions[0].BreakProduction();
+                        toolStripMenuItemStartA.Enabled = false;
+                        toolStripMenuItemStopA.Enabled = false;
+                        toolStripMenuItemContinueA.Enabled = true;
+                        break;
+                    }
+                case "toolStripMenuItemStopB":
+                    {
+                        woodBoxProductions[1].BreakProduction();
+                        toolStripMenuItemStartB.Enabled = false;
+                        toolStripMenuItemStopB.Enabled = false;
+                        toolStripMenuItemContinueB.Enabled = true;
+                        break;
+                    }
+                case "toolStripMenuItemStopC":
+                    {
+                        woodBoxProductions[2].BreakProduction();
+                        toolStripMenuItemStartC.Enabled = false;
+                        toolStripMenuItemStopC.Enabled = false;
+                        toolStripMenuItemContinueC.Enabled = true;
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// continue the production that the user make a break
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItemResume_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)sender;
+            switch (menuItem.Name)
+            {
+                case "toolStripMenuItemContinueA":
+                    {
+                        woodBoxProductions[0].ResumeProduction();
+                        toolStripMenuItemStartA.Enabled = false;
+                        toolStripMenuItemStopA.Enabled = true;
+                        toolStripMenuItemContinueA.Enabled = false;
+                        break;
+                    }
+                case "toolStripMenuItemContinueB":
+                    {
+                        woodBoxProductions[1].ResumeProduction();
+                        toolStripMenuItemStartB.Enabled = false;
+                        toolStripMenuItemStopB.Enabled = true;
+                        toolStripMenuItemContinueB.Enabled = false;
+                        break;
+                    }
+                case "toolStripMenuItemContinueC":
+                    {
+                        woodBoxProductions[2].ResumeProduction();
+                        toolStripMenuItemStartC.Enabled = false;
+                        toolStripMenuItemStopC.Enabled = true;
+                        toolStripMenuItemContinueC.Enabled = false;
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// terminate the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            foreach (WoodBoxProduction production in woodBoxProductions)
+            {
+                production.WoodBoxNumberOfBoxProductedChanged -= woodBoxNumberOfBoxProductedChanged;
+                production.CancelProduction();
+            }
+            this.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        private void woodBoxNumberOfBoxProductedChanged(WoodBoxProduction sender)
+        {
+            this.Invoke(new UpdateIHMDelegate(UpdateIhmNumberOfBox), new object[] { sender });
+        }
+
+        private void woodBoxProductiontStateChanged(WoodBoxProduction sender, WoodBoxProduction.ProductionState state)
+        {
+            if(state == WoodBoxProduction.ProductionState.finished)
+            {
+                if(sender == woodBoxProductions[0])
+                {
+                    woodBoxProductions[0] = new WoodBoxProduction(10000, 277, "Type A");
+                    toolStripMenuItemStartA.Enabled = true;
+                    toolStripMenuItemStopA.Enabled = false;
+                    toolStripMenuItemContinueA.Enabled = false;
+                }
+                else if(sender == woodBoxProductions[1])
+                {
+                    woodBoxProductions[1] = new WoodBoxProduction(25000, 55, "Type B");
+                    toolStripMenuItemStartB.Enabled = true;
+                    toolStripMenuItemStopB.Enabled = false;
+                    toolStripMenuItemContinueB.Enabled = false;
+                }
+                else if(sender == woodBoxProductions[2])
+                {
+                    new WoodBoxProduction(120000, 2, "Type C");
+                    toolStripMenuItemStartC.Enabled = true;
+                    toolStripMenuItemStopB.Enabled = false;
+                    toolStripMenuItemContinueC.Enabled = false;
+                }
+            }
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_Closing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult applicationExitDialog = MessageBox.Show("Voulez vous vraiment quitter l'application", "Quitter", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (applicationExitDialog == DialogResult.No && sender == this)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                e.Cancel = true;
+                foreach (WoodBoxProduction production in woodBoxProductions)
+                {
+                    production.WoodBoxNumberOfBoxProductedChanged -= woodBoxNumberOfBoxProductedChanged;
+                    production.CancelProduction();
+                }
+                e.Cancel = false;
+            }
         }
     }
 }
