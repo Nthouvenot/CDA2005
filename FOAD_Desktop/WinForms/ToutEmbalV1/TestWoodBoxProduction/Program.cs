16d081efffcc39f2f99e6f1c5eb76dfa08f5cf7e
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-12-04 10:42:47 +0100

    Create TestWoodBoxProduction
    test de la classe metier WoodBoxProduction

---
+++ FOAD_Desktop/WinForms/ToutEmbalV1/TestWoodBoxProduction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibraryToutEmbal;
+
+namespace TestWoodBoxProduction
+{
+    class Program
+    {
+        static WoodBoxProduction production;
+
+        static void Main(string[] args)
+
+        {
+             production = new WoodBoxProduction(5000, 50, "B");
+             production.WoodBoxNumberOfBoxProductedChanged += Production_WoodBoxNumberOfBoxProductedChanged;
+             production.WoodBoxProductiontStateChanged += Production_WoodBoxProductiontStateChanged;
+             production.StartProduction();
+             
+             Console.ReadLine();
+        }
+
+        private static void Production_WoodBoxProductiontStateChanged(WoodBoxProduction sender, WoodBoxProduction.ProductionState state)
+        {
+            Console.WriteLine("état de la production : " + state);
+        }
+
+        private static void Production_WoodBoxNumberOfBoxProductedChanged(WoodBoxProduction sender)
+        {
+            Console.WriteLine(production.numberOfDefault.ToString());
+            Console.WriteLine(production.TotalNumberOfBoxWithAndWithoutDefault.ToString());
+        }
+    }
+}
