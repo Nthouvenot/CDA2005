c74b3ec0c1ab45d67794ce6af629a073fdce00a4
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-12-16 18:07:30 +0100

    Create Papyrus/ClassLibraryPapyrus
    bibliotheque de classe de validation

---
+++ FOAD_Desktop/Ado/ExercicePapyrus/ClassLibraryPapyrus/CheckTools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPapyrus
+{
+    public static class CheckTools
+    {
+        public static bool CheckSuplierCode(string suplierCode)
+        {
+            Regex suplierCodeRegex = new Regex(@"^[0-9]{1,10}$");
+            if (!suplierCodeRegex.IsMatch(suplierCode))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
