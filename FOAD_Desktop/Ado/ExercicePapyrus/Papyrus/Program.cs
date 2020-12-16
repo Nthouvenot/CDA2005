63d1eeb0e84d86eaa9c48fe6a22f128b3e8f49ea
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-12-16 18:06:12 +0100

    Update papyrus
    appel de la fenetre PrintUpdateDeleteSupplierDataWindow

--- FOAD_Desktop/Ado/ExercicePapyrus/Papyrus/Program.cs
+++ FOAD_Desktop/Ado/ExercicePapyrus/Papyrus/Program.cs
@@ -16,7 +16,7 @@ namespace Papyrus
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new OrderListWindow());
+            Application.Run(new PrintUpdateDeleteSupplierDataWindow());
         }
     }
 }
