97f5f8c9cb8f206ac359f8999e8f4894a132166d
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-12-21 15:53:15 +0100

    Create ./SolutionItems/
    dossier pour les script sql

---
+++ FOAD_Desktop/Ado/ExercicePapyrus/SolutionItems/SqlCreateStoredProcedure.sql
@@ -0,0 +1,30 @@
+USE PapyrusNicolas;
+GO  
+
+ DROP TABLE IF EXISTS GetCommandeBySuplier;
+ GO
+
+CREATE PROCEDURE GetCommandeBySuplier  
+    @suplier_number int   
+AS   
+    SELECT Commandes.NUMERO_COMMANDE ,Commandes.DATE_COMMANDE, Commandes.OBSERVATION_COMMANDE
+    FROM Liste_Commandes, Produits_a_Vendre, Fournisseurs, Commandes
+    WHERE Fournisseurs.NUMERO_FOURNISSEUR = Produits_a_Vendre.NUMERO_FOURNISSEUR AND Liste_Commandes.CODE_ARTICLE = Produits_a_Vendre.CODE_ARTICLE AND Liste_Commandes.NUMERO_COMMANDE = Commandes.NUMERO_COMMANDE AND Fournisseurs.NUMERO_FOURNISSEUR = @suplier_number;
+GO  
+
+DROP TABLE IF EXISTS WriteSuplierTable;
+ GO
+
+CREATE PROCEDURE WriteSuplierTable
+    @name nvarchar,
+    @street_number int,
+    @street nvarchar,
+    @zip_code int,
+    @city nvarchar,
+    @contact nvarchar,
+    @satisfaction int
+AS
+    INSERT INTO Fournisseurs(NOM_FOURNISSEUR, NUMERO_RUE_FOURNISSEUR, RUE_FOURNISSEUR, CODE_POSTAL_FOURNISSEUR, VILLE_FOURNISSEUR, CONTACT_FOURNISSEUR, SATISFACTION_FOURNISSEUR)
+    VALUES (@name, @street_number, @street, @zip_code, @city, @contact, @satisfaction);
+    RETURN (SELECT @@identity);
+GO
