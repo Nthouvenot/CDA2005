f4e8eaf8e8ef4f5103c81e4ab3f1d47c7ed6db1b
Nicolas Thouvenot <thouvenot_nicolas@outlook.fr>
2021-01-22 10:52:48 +0100

    Create DesignPatternCompositeAdditionSubstraction
    reprise de l'exercice de l'addition avec le design pattern composite et ajout soustraction

---
+++ FOAD_DesignPattern/DesignPatternCompositeAdditionSubstraction/CompositeLibrary/Substraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeLibrary
+{
+    public class Substraction : Binary
+    {
+        public Substraction(Expression leftExpression, Expression rightExpression) :base(leftExpression, rightExpression)
+        {
+
+        }
+
+        public override int Evaluate()
+        {
+            return  this.leftExpression - this.rightExpression;
+        }
+
+        public override string Format()
+        {
+            if (this.leftExpression is NumberExpression && this.rightExpression is NumberExpression)
+            {
+                return this.leftExpression.Format() + "-" + this.rightExpression.Format() + "=" + this.Evaluate().ToString();
+            }
+            else
+            {
+                return this.leftExpression.Format() + "-" + this.rightExpression.Format();
+            }
+        }
+    }
+}
