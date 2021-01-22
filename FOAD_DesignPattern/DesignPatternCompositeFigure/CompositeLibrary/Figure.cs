4467f9bd04a4592db2a51886ae32d357b8a3340b
Nicolas Thouvenot <thouvenot_nicolas@outlook.fr>
2021-01-22 12:10:15 +0100

    Create DesignPatternCompositeFigure
    démarrage de l'exercice Figure sur le design pattern composite

---
+++ FOAD_DesignPattern/DesignPatternCompositeFigure/CompositeLibrary/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeLibrary
+{
+    public abstract class Figure
+    {
+        private Coordinate coordinate;
+
+        public Figure(Coordinate coordinate)
+        {
+            this.coordinate = new Coordinate(coordinate);
+        }
+
+        public abstract void Draw();
+    }
+}
