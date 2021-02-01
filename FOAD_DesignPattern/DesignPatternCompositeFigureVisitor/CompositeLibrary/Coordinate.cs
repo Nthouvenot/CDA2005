1a3cdafd0983b81345c881ea3c71179c87e8733d
Nicolas Thouvenot <thouvenot_nicolas@outlook.fr>
2021-02-01 15:28:56 +0100

    Update DesignPatternCompositeFigureVisitor
    implémentation des méthodes visit pour dessiner dans la Form

--- FOAD_DesignPattern/DesignPatternCompositeFigureVisitor/CompositeLibrary/Coordinate.cs
+++ FOAD_DesignPattern/DesignPatternCompositeFigureVisitor/CompositeLibrary/Coordinate.cs
@@ -9,6 +9,9 @@ namespace CompositeLibrary
         private int xCoordinnate;
         private int yCoordinnate;
 
+        public int XCoordinnate { get => xCoordinnate; protected set => xCoordinnate = value; }
+        public int YCoordinnate { get => yCoordinnate; protected set => yCoordinnate = value; }
+
         public Coordinate(int xCoordinnate, int yCoordinnate)
         {
             this.xCoordinnate = xCoordinnate;
